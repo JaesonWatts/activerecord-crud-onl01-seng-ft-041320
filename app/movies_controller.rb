cd73c918a03e946231d097036f6e43e73d267a89
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-25 02:56:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,8 +28,9 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # title == "Home Alone"
   # release_date == 1990
   
-  Movie.create do |m|
-    m = Movie.new
+  Movie.create do |movie|
+    movie.title = args[:title]
+    movie.release_date = args[:release_date]
   end
 end
 
